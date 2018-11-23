d41de6ac901ede1bb31973eb81f1ddd8fb860bca
Damian Godek <damian.godek1@gmail.com>
2018-11-23 13:11:02 +0100

    add login function

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '6643659411-uqcd8uvohm4jchi725k6tqg85qh1oula.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'Vu6WuuOW5csHJFgHqcKuDtR-',
+	'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
+
+/*
+869408879731-gqc1teg4hu0cj780tj8vttv8nof4m6is.apps.googleusercontent.com
+1wKFlu1xXc6FHgtpQlI7d7Ww
+*/
